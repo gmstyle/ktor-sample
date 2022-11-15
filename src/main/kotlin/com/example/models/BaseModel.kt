ff6c9900b7a3ba8803595a4ba8af45aa47cfe149
Gabriele Martina <contattagmstyle@gmail.com>
2022-11-15 16:53:25 +0100

    update dipendenze

--- src/main/kotlin/com/example/models/BaseModel.kt
+++ src/main/kotlin/com/example/models/BaseModel.kt
@@ -2,7 +2,7 @@ package com.example.models
 
 import org.ktorm.entity.Entity
 
-interface BaseModel<T : Entity<T>, R> {
+interface BaseModel<T: Entity<T>, R> {
 
     fun entityToObject(entity: T) : R
 
